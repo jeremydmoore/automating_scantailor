dad75868b3bfe9d5a4c7b969affd5f43f0e1e382
Jeremy Moore <alt.photosbyjeremy@gmail.com>
2020-07-02 11:47:02 -0400

    handle continuous tone images
    add notebook to process the continuous tone images after scantailor output

--- scripts/python/config.py
+++ scripts/python/config.py
@@ -7,7 +7,10 @@ root_dir_path = Path('/Users/dlisla/shared_with_docker')
 step1_dir_path = root_dir_path.joinpath('1.toScanTailor')
 
 # have been ScanTailored, a holding directory
-step2_dir_path = root_dir_path.joinpath('2.ScanTailored')
+step2_dir_path = root_dir_path.joinpath('2.ScanTailored-holding')
 
 # need quality control
 step3_dir_path = root_dir_path.joinpath('3.toQC')
+
+collection_stub = 'agrutesc'
+collection_dir_path = root_dir_path.joinpath(collection_stub)
--- scripts/python/move_scantailored_volume.py
+++ scripts/python/move_scantailored_volume.py
@@ -5,37 +5,39 @@ from shutil import copy2
 import applescript
 import config
 
-def move_dir(volume_dir_path):
-    volume_new_dir_path = config.step2_dir_path.joinpath(volume_dir_path.name)
-
-    cmd = f'{volume_dir_path.name}\n -- will become --> \n{volume_new_dir_path}'
-    applescript.display_notification(cmd)
+def move_dir(to_move_dir_path):
+    new_dir_path = config.step2_dir_path.joinpath(to_move_dir_path.name)
 
     # move directory with rename
-    volume_dir_path.rename(volume_new_dir_path)
+    to_move_dir_path.rename(new_dir_path)
 
-    if volume_new_dir_path.is_dir():
-        volume_dir_path = volume_new_dir_path
+    if new_dir_path.is_dir():
+        volume_dir_path = new_dir_path
     else:
-        raise ValueError(f'{volume_new_dir_path} is not a directory')
+        raise ValueError(f'{new_dir_path} is not a directory')
 
     # get list of images in out directory
     tif_path_list = sorted(volume_dir_path.joinpath('out').glob('*.tif'))
 
-    volume_new_dir_path = config.step3_dir_path.joinpath(volume_dir_path.name)
-    volume_new_dir_path.mkdir()
+    new_dir_path = config.step3_dir_path.joinpath(volume_dir_path.name)
+    new_dir_path.mkdir()
 
     # copy out tifs into new directory in step3_dir_path
     for tif_path in tif_path_list:
-        new_tif_path = volume_new_dir_path.joinpath(tif_path.name)
+        new_tif_path = new_dir_path.joinpath(tif_path.name)
         copy2(tif_path, new_tif_path)
+
+    cmd = f'{to_move_dir_path.name} \
+            ---> \
+            {new_dir_path}'
+    applescript.display_notification(cmd)
 
 # get list of volumes in directory
 volume_dir_path_list = sorted([x for x in config.step1_dir_path.iterdir() if x.is_dir()])
 
 # grab 1 volume, which is the first 1 and move it
-volume_dir_path = volume_dir_path_list[0]
-move_dir(volume_dir_path)
+volume_to_move = volume_dir_path_list[0]
+move_dir(volume_to_move)
 
 # for batch processing
 # for volume_dir_path in volume_dir_path_list:
