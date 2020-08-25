cc6232181204616c954c90f2fc1991dcc07d3b25
Jeremy Moore <alt.photosbyjeremy@gmail.com>
2020-08-24 20:49:21 -0400

    add classes
    Started using JupyterLab. Started building system for processing ScanTailored files more quickly.

--- scripts/python/config.py
+++ scripts/python/config.py
@@ -19,12 +19,20 @@ step2_dir_path = project_dir_path.joinpath('2.ScanTailored-holding')
 # need quality control
 step3_dir_path = project_dir_path.joinpath('3.toQC')
 
-# list of dir_paths in config.py
-dir_path_list = [root_dir_path, project_dir_path, step1_dir_path, step2_dir_path, step3_dir_path]
+# dict of dir_paths in config.py
+dir_path_dict = {'root': root_dir_path,
+                 'project': project_dir_path,
+                 'step1': step1_dir_path,
+                 'step2': step2_dir_path,
+                 'step3': step3_dir_path,
+                }
 
 def verify():
     print(f'Verifying {len(dir_path_list)} workflow directories for Project "{project_identifier}" . . .')
-    for dir_path in dir_path_list:
+    for dir_type, dir_path in dir_path_dict.items():
         if not dir_path.is_dir():
             raise ValueError(f'{dir_path} is not a directory')
-    print(f'\t{len(dir_path_list)} directories verified successfully. Godspeed, my child.')
+        else:
+            print(f'\t\t{dir_type}: {dir_path}')
+    # add spaces equal to "Verifying " at beginning so # of directories lines up
+    print(f'{" " * 10}{len(dir_path_list)} workflow directories verified successfully')
